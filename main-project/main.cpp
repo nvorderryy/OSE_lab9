bb3b903f3734b56dac1e595ef3dd01f915ae4b6a
nvorderryy <cobrazmeika3@gmail.com>
2024-12-11 00:22:34 +0300

    "Добавлен вывод общей информации о задании"

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #5. Internet protocol\n";
+    cout << "Author: Egor Kuznechik\n";
     return 0;
 }
